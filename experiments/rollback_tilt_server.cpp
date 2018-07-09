2c06c25b558eda33cb1d36782357734c9827cabb
Sylvain Gadrat <sgadrat@wontfix.it>
2018-07-09 16:58:40 +0200

    experiments: Rollback server handles messages in batches

--- experiments/rollback_tilt_server.cpp
+++ experiments/rollback_tilt_server.cpp
@@ -65,108 +65,119 @@ int main() {
 	};
 	uint8_t prediction_id = 0;
 	while (true) {
-		// Get next incomming message
-		try {
-			in_message = in_messages.pop();
-		}catch(std::exception const& e) {
-			in_message = nullptr;
+		// Reconstruct gamestate history according to bufferized messages
+		bool handled_message = false;
+		while (true) {
+			// Get next incomming message
+			try {
+				in_message = in_messages.pop();
+				handled_message = true;
+			}catch(std::exception const& e) {
+				in_message = nullptr;
+			}
+
+			// Process message
+			if (in_message == nullptr) {
+				break;
+			}else {
+				// Add the sender to the clients list
+				bool found = false;
+				for (boost::asio::ip::udp::endpoint const& client_endpoint : clients) {
+					if (client_endpoint.address() == in_message->sender.address() && client_endpoint.port() == in_message->sender.port()) {
+						found = true;
+						break;
+					}
+				}
+
+				if (!found) {
+					std::cout << "new client: " << in_message->sender.address() << ":" << in_message->sender.port() << std::endl;
+					clients.push_back(in_message->sender);
+				}
+
+				// Print received message
+				std::cout << "got a message from " << in_message->sender.address() << ":" << in_message->sender.port() << std::endl;
+				std::cout << in_message->data.size() << " B" << std::endl;
+				std::ostringstream oss;
+				oss << std::hex;
+				for (uint8_t b : in_message->data) {
+					oss << std::setfill('0') << std::setw(2) << (uint16_t)b << ' ';
+				}
+				std::cout << oss.str() << std::endl;
+
+				// Parse message
+				stnp::message::ControllerState message;
+				stnp::message::MessageDeserializer deserializer(in_message->data);
+				message.serial(deserializer);
+
+				// Invalidate gamestate history from message's timestamp
+				std::map<uint32_t, GameState>::iterator first_invalid_gamestate(gamestate_history.lower_bound(message.timestamp));
+				if (first_invalid_gamestate != gamestate_history.end()) {
+					if (first_invalid_gamestate->first == 0) {
+						std::cerr << "error: prevented modification of initial gamestate" << std::endl;
+					}else {
+						gamestate_history.erase(first_invalid_gamestate, gamestate_history.end());
+					}
+				}
+
+				// Rewrite input history from message's timestamp
+				GameState::ControllerState controller_state = controller_state_from_message(message);
+				std::map<uint32_t, GameState::ControllerState>* controller_history = nullptr;
+				if (message.client_id == 0) {
+					controller_history = &controller_a_history;
+				}else {
+					controller_history = &controller_b_history;
+				}
+
+				(*controller_history)[message.timestamp] = controller_state;
+				for (
+					std::map<uint32_t, GameState::ControllerState>::iterator controller_history_entry(++controller_history->find(message.timestamp));
+					controller_history_entry != controller_history->end();
+					++controller_history_entry
+				)
+				{
+					controller_history_entry->second = controller_state;
+				}
+
+				// Compute gamestate at the last input in history
+				{
+					uint32_t gamestate_time = gamestate_history.rbegin()->first;
+					GameState gamestate = gamestate_history.rbegin()->second;
+					uint32_t last_input_time = std::max(
+						controller_a_history.rbegin()->first,
+						controller_b_history.rbegin()->first
+					);
+
+					while (gamestate_time < last_input_time + 1) {
+						// Apply inputs
+						//  TODO check if it is more accurate to apply inputs before or after the simulation's tick
+						std::map<uint32_t, GameState::ControllerState>::const_iterator controller_history_entry(controller_a_history.find(gamestate_time));
+						if (controller_history_entry != controller_a_history.end()) {
+							gamestate.setControllerAState(controller_history_entry->second);
+						}
+
+						controller_history_entry = controller_b_history.find(gamestate_time);
+						if (controller_history_entry != controller_b_history.end()) {
+							gamestate.setControllerBState(controller_history_entry->second);
+						}
+
+						// Tick Simulation
+						gamestate.tick();
+						++gamestate_time;
+					}
+
+					gamestate_history.insert(std::pair<uint32_t, GameState>(gamestate_time, gamestate));
+				}
+
+				// Increment prediction ID
+				++prediction_id;
+
+				// Break reference to incoming message
+				in_message = nullptr;
+			}
 		}
 
-		if (in_message != nullptr) {
-			// Add the sender to the clients list
-			bool found = false;
-			for (boost::asio::ip::udp::endpoint const& client_endpoint : clients) {
-				if (client_endpoint.address() == in_message->sender.address() && client_endpoint.port() == in_message->sender.port()) {
-					found = true;
-					break;
-				}
-			}
-
-			if (!found) {
-				std::cout << "new client: " << in_message->sender.address() << ":" << in_message->sender.port() << std::endl;
-				clients.push_back(in_message->sender);
-			}
-
-			// Print received message
-			std::cout << "got a message from " << in_message->sender.address() << ":" << in_message->sender.port() << std::endl;
-			std::cout << in_message->data.size() << " B" << std::endl;
-			std::ostringstream oss;
-			oss << std::hex;
-			for (uint8_t b : in_message->data) {
-				oss << std::setfill('0') << std::setw(2) << (uint16_t)b << ' ';
-			}
-			std::cout << oss.str() << std::endl;
-
-			// Parse message
-			stnp::message::ControllerState message;
-			stnp::message::MessageDeserializer deserializer(in_message->data);
-			message.serial(deserializer);
-
-			// Invalidate gamestate history from message's timestamp
-			std::map<uint32_t, GameState>::iterator first_invalid_gamestate(gamestate_history.lower_bound(message.timestamp));
-			if (first_invalid_gamestate != gamestate_history.end()) {
-				if (first_invalid_gamestate->first == 0) {
-					std::cerr << "error: prevented modification of initial gamestate" << std::endl;
-				}else {
-					gamestate_history.erase(first_invalid_gamestate, gamestate_history.end());
-				}
-			}
-
-			// Rewrite input history from message's timestamp
-			GameState::ControllerState controller_state = controller_state_from_message(message);
-			std::map<uint32_t, GameState::ControllerState>* controller_history = nullptr;
-			if (message.client_id == 0) {
-				controller_history = &controller_a_history;
-			}else {
-				controller_history = &controller_b_history;
-			}
-
-			(*controller_history)[message.timestamp] = controller_state;
-			for (
-				std::map<uint32_t, GameState::ControllerState>::iterator controller_history_entry(++controller_history->find(message.timestamp));
-				controller_history_entry != controller_history->end();
-				++controller_history_entry
-			)
-			{
-				controller_history_entry->second = controller_state;
-			}
-
-			// Compute gamestate at the last input in history
-			{
-				uint32_t gamestate_time = gamestate_history.rbegin()->first;
-				GameState gamestate = gamestate_history.rbegin()->second;
-				uint32_t last_input_time = std::max(
-					controller_a_history.rbegin()->first,
-					controller_b_history.rbegin()->first
-				);
-
-				while (gamestate_time < last_input_time + 1) {
-					// Apply inputs
-					//  TODO check if it is more accurate to apply inputs before or after the simulation's tick
-					std::map<uint32_t, GameState::ControllerState>::const_iterator controller_history_entry(controller_a_history.find(gamestate_time));
-					if (controller_history_entry != controller_a_history.end()) {
-						gamestate.setControllerAState(controller_history_entry->second);
-					}
-
-					controller_history_entry = controller_b_history.find(gamestate_time);
-					if (controller_history_entry != controller_b_history.end()) {
-						gamestate.setControllerBState(controller_history_entry->second);
-					}
-
-					// Tick Simulation
-					gamestate.tick();
-					++gamestate_time;
-				}
-
-				gamestate_history.insert(std::pair<uint32_t, GameState>(gamestate_time, gamestate));
-			}
-
-			// Increment prediction ID
-			++prediction_id;
-
-			// Break reference to incoming message
-			in_message = nullptr;
-
+		// Handle history change
+		if (handled_message) {
 			// Send the new game state to all clients
 			stnp::message::NewGameState gamestate;
 
@@ -194,7 +205,7 @@ int main() {
 			}
 			ofs << ' ' << controller_b_history.size();
 			for (std::pair<uint32_t, GameState::ControllerState> const& entry : controller_b_history) {
-				ofs << ' ' << entry.first << ' ' << (uint16_t)entry.second.getRaw();
+					ofs << ' ' << entry.first << ' ' << (uint16_t)entry.second.getRaw();
 			}
 		}
 
