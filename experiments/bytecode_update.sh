4f5d562d85d17b40239caec11638e8fe6c0a7386
Sylvain Gadrat <sgadrat@wontfix.it>
2020-12-25 14:59:27 +0100

    bytecode update: current directory independance

--- experiments/bytecode_update.sh
+++ experiments/bytecode_update.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+base=`dirname $0`
+
 # Input files (must exists)
 BYTECODE_PATH=~/workspace/nes/tilt/server_bytecode.nes
 XA_LISTING_PATH=/tmp/server_bytecode.lst
-KNOWN_OPCODES_PATH=compile_bytecode/known_opcodes.json
+KNOWN_OPCODES_PATH="$base/compile_bytecode/known_opcodes.json"
 
 # Ouptput/intermediate files (will be overwriten)
 ENTRY_POINTS_PATH=/tmp/entries.json
-CPP_BYTECODE_PATH=../src/GameState.bytecodedata.inc.cpp
-CPP_COMPILED_PATH=../src/GameState.bytecodecompiled.inc.cpp
+CPP_BYTECODE_PATH="$base/../src/GameState.bytecodedata.inc.cpp"
+CPP_COMPILED_PATH="$base/../src/GameState.bytecodecompiled.inc.cpp"
 
 # Get bytecode in a C++ array
-./convert_bytecode.sh "$BYTECODE_PATH" > "$CPP_BYTECODE_PATH"
+"$base/convert_bytecode.sh" "$BYTECODE_PATH" > "$CPP_BYTECODE_PATH"
 
 # Get pre-filled JIT structure
-compile_bytecode/extract_entry_points.py "$XA_LISTING_PATH" > "$ENTRY_POINTS_PATH"
-compile_bytecode/compile_bytecode.py "$BYTECODE_PATH" "$ENTRY_POINTS_PATH" "$KNOWN_OPCODES_PATH" > "$CPP_COMPILED_PATH"
+"$base/compile_bytecode/extract_entry_points.py" "$XA_LISTING_PATH" > "$ENTRY_POINTS_PATH"
+"$base/compile_bytecode/compile_bytecode.py" "$BYTECODE_PATH" "$ENTRY_POINTS_PATH" "$KNOWN_OPCODES_PATH" > "$CPP_COMPILED_PATH"
