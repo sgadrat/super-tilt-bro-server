ab41e3a6ddcfafa563a1bd1cde6560d6be287844
Sylvain Gadrat <sgadrat@wontfix.it>
2021-05-19 10:49:17 +0200

    server: reject clients below 2.0-alpha7

--- src/server/InitializationHandler.cpp
+++ src/server/InitializationHandler.cpp
@@ -247,7 +247,7 @@ void InitializationHandler::run() {
 						in_message->sender
 					);
 #ifndef BETA_SERVER
-				}else if (getVersionTuple(connection_request) < std::make_tuple(2, stnp::message::Connection::ReleaseType::ALPHA, 6)) {
+				}else if (getVersionTuple(connection_request) < std::make_tuple(2, stnp::message::Connection::ReleaseType::ALPHA, 7)) {
 #else
 				}else if (getVersionTuple(connection_request) > std::make_tuple(0, stnp::message::Connection::ReleaseType::ALPHA, 0)) {
 #endif
