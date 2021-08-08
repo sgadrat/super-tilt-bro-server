94f83c763a4b40c53b07fa8f58474e10eb85a353
Sylvain Gadrat <sgadrat@wontfix.it>
2021-08-08 23:17:58 +0200

    replay_server: use original characters palettes

--- replay_server/replaydb.py
+++ replay_server/replaydb.py
@@ -58,7 +58,7 @@ def push_games(games_info):
 	# Record games
 	for game_info in games_info:
 		# Check game consistency
-		mandatory_fields = ['bmov', 'game', 'begin', 'character_a', 'character_b']
+		mandatory_fields = ['bmov', 'game', 'begin', 'character_a', 'character_b', 'character_a_palette', 'character_b_palette']
 		for field in mandatory_fields:
 			if field not in game_info:
 				raise Exception('invalid game info format, missing "{}" field'.format(field))
@@ -74,7 +74,12 @@ def push_games(games_info):
 
 		# Convert bmov to fm2
 		fm2_path = get_fm2_path(game_info['game'])
-		cmd = [_bmov_to_fm2, bmov_path]
+		cmd = [
+			_bmov_to_fm2,
+			'--palette-a', str(game_info['character_a_palette']),
+			'--palette-b', str(game_info['character_b_palette']),
+			bmov_path
+		]
 		fm2_data = subprocess.run(cmd, check=True, encoding='utf-8', stdout=subprocess.PIPE).stdout
 		with open(fm2_path, 'w') as fm2_file:
 			fm2_file.write(fm2_data)
@@ -88,6 +93,9 @@ def push_games(games_info):
 			'begin': game_info['begin'],
 			'character_a': game_info['character_a'],
 			'character_b': game_info['character_b'],
+			'character_a_palette': game_info['character_a_palette'],
+			'character_b_palette': game_info['character_b_palette'],
+			'stage': game_info['stage']
 		}
 
 	# Update DB file
