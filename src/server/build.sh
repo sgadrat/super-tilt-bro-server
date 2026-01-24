556715bfdaf5b31c77e1e26ca2351d1276ffd39b
Sylvain Gadrat <sgadrat@wontfix.it>
2026-01-24 23:15:51 +0100

    server: do not link with obsolete boost_system if not necessary
    boost_system was removed as of boost 1.89

--- src/server/build.sh
+++ src/server/build.sh
@@ -4,6 +4,7 @@ dbg_log_flags=""
 optim_flags="-O3 -DNDEBUG -flto"
 bin_suffix=""
 beta_flags=""
+extra_link_flags=""
 if [ "x$1" == "xdbg" ]; then
 	dbg_log_flags="-DLOG_FLOOD"
 	optim_flags="-O0 -g"
@@ -11,6 +12,14 @@ fi
 if [ ! -z "$BETA_SERVER" ]; then
 	bin_suffix="_beta"
 	beta_flags="-DBETA_SERVER"
+fi
+
+# Hack checking distrib name to know if it can link with boost_system
+#  Better check if the link with boost_system is still necessary in other distribs
+if grep archlinux /proc/version > /dev/null 2>&1 ; then
+	extra_link_flags=""
+else
+	extra_link_flags="-lboost_system"
 fi
 
 g++ \
@@ -26,4 +35,4 @@ g++ \
 	StatisticsSink.cpp \
 	-lpthread \
 	-luuid \
-	-lboost_system
+	$extra_link_flags
